14d1c0927f2f7e570199c08b6e0f592c078a273a
Pas92 <pavelburdavitsyn92@gmail.com>
2022-02-21 23:00:03 +0300

    style: add styles for header

--- src/components/header/header.ts
+++ src/components/header/header.ts
@@ -6,6 +6,18 @@ import "./header.scss";
 export const header = htmlToElement(HTML);
 const authLink = header?.querySelector('.nav__auth') as HTMLLinkElement | null
 
+const links = header?.querySelector('ul')
+links?.addEventListener('click', (e: Event) => {
+  const target = e.target as HTMLElement
+  if (target.classList.contains('header__link')) {
+    Array.from(links.children).forEach((e) => {
+      e.classList.remove('active')
+    })
+
+    target.parentElement?.classList.add('active')
+  }
+})
+
 const changeLinkToLogout = () => {
   if (authLink) {
     authLink.textContent = 'Выйти'
